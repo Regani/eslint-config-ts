1781f1faa35b9c458d96270d3b1c066fdd5cd003
Bohdan Konechniy <bohdan.konechniy@gmail.com>
2022-09-18 12:47:41 +0300

    Fix

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-/* eslint-env node */
-module.exports = {
-    root: true,
-    plugins: [
-        "@typescript-eslint"
-    ],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'eslint:recommended'
-    ],
-    rules: {
-        '@typescript-eslint/ban-ts-comment': [ 'error' ],
-        '@typescript-eslint/no-unused-vars': 'error',
-        'no-unused-vars': 'off',
-        'no-console': 'error',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'semi': [ 'error', 'never' ],
-        'quotes': [ 'error', 'single' ],
-        'object-curly-spacing': [ 'error', 'always' ],
-        'array-bracket-spacing': [ 'error', 'always' ],
-        'space-before-function-paren': [ 'error', 'always' ],
-        '@typescript-eslint/no-var-requires': 0,
-        'indent': [ 'error', 4 ],
-    },
-    env: {
-        es2021: true,
-        node: true
-    }
-}
+const eslintrc = require('./.eslintrc.js')
+
+module.exports = eslintrc
